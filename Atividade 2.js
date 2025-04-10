c127cf95c8113c3b88ad69bb5d66035aa422e568
Rafael35jguyyytdtf <rafaelbsantos1411@gmail.com>
2025-04-10 11:41:36 -0400

    Atividade 2

---
+++ Atividade 2.js
@@ -0,0 +1,22 @@
+//1)
+var num1 = 3 + 5;
+var num2 = 3 - 5;
+var num3 = 3 * 5;
+var num4 = 3 / 5;
+console.log(`resultado ${num1} ${num2} ${num3} ${num4}`);
+
+//2)
+let p1 = 17;
+let p2 = 14;
+let idade = p1 == p2 ? "Eles tem a amesma idade" : "Eles não tem a mesma idade"
+console.log(idade);
+
+//3)
+let idade = 18;
+let maior_idade = idade >= 18 ? "Você e maior de idade" : "Você e menor de idade";
+console.log(maior_idade);
+
+//4)
+let num = 6;
+let verif_numero = num == 6 ?"Esse número e par" : "Esse número e impar";
+console.log(verif_numero);
