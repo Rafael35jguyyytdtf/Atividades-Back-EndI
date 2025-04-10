e1d3086082bc1247141ac5dbd4df60398b0ea3e9
Rafael35jguyyytdtf <rafaelbsantos1411@gmail.com>
2025-04-10 11:10:04 -0400

    Atividades Back-End

---
+++ Atividades.js
@@ -0,0 +1,23 @@
+//1)
+nome = "Rafael";
+let idade = 17;
+
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos`);
+//2)
+let texto = "Olá, Mundo!";
+
+let maiusculas = texto.toUpperCase();
+console.log("Em maiúsculas:", maiusculas);
+
+let minusculas = texto.toLowerCase();
+console.log("Em minúsculas:", minusculas);
+
+//3)
+var valor = 17;
+console.log (typeof valor);
+
+//4)
+altura = 1.75;
+let peso = 70;
+let imc = peso / (altura * altura);
+console.log(`Seu IMC é: ${imc.toFixed(2)}`);
